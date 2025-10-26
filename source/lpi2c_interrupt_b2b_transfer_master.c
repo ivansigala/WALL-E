33bc62bc9f4f89ffa98e01971dd2a9f8bd9d446f
DavidBAV051 <davidbarragan051@gmail.com>
2025-10-25 18:25:50 -0600

    Implemented Calibration Logic, will be better optimized

--- source/lpi2c_interrupt_b2b_transfer_master.c
+++ source/lpi2c_interrupt_b2b_transfer_master.c
@@ -1,11 +1,3 @@
-/*
- * Copyright 2017 NXP
- * All rights reserved.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-/*  Standard C Included Files */
 #include <stdio.h>
 #include <string.h>
 #include "board.h"
@@ -13,185 +5,128 @@
 #include "fsl_lpi2c.h"
 #include "app.h"
 
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
+#define MPU9250_ADDR        0x68
+#define WHO_AM_I_REG        0x75
+#define PWR_MGMT_1_REG      0x6B
+#define ACCEL_XOUT_H        0x3B
+#define I2C_BAUDRATE        100000U
+#define LPI2C_MASTER_BASE   ((LPI2C_Type *)EXAMPLE_I2C_MASTER_BASE) // Adjust based on your board
+#define CALIB_SAMPLE		1000U
 
-#define EXAMPLE_I2C_MASTER ((LPI2C_Type *)EXAMPLE_I2C_MASTER_BASE)
-
-#define I2C_MASTER_SLAVE_ADDR_7BIT 0x68 //0x7EU
-#define I2C_BAUDRATE               100000U
-#define I2C_DATA_LENGTH            33U
-
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
-
-/*******************************************************************************
- * Variables
- ******************************************************************************/
-
-uint8_t g_master_txBuff[I2C_DATA_LENGTH];
-uint8_t g_master_rxBuff[I2C_DATA_LENGTH];
 lpi2c_master_handle_t g_m_handle;
 volatile bool g_MasterCompletionFlag = false;
-volatile bool g_MasterNackFlag       = false;
+volatile bool g_MasterNackFlag = false;
 
-/*******************************************************************************
- * Code
- ******************************************************************************/
+volatile int32_t ax_sum = 0, ay_sum = 0, az_sum = 0, gx_sum = 0, gy_sum = 0, gz_sum = 0, temp_sum = 0;
 
+/* Callback */
 static void lpi2c_master_callback(LPI2C_Type *base, lpi2c_master_handle_t *handle, status_t status, void *userData)
 {
     if (status == kStatus_LPI2C_Nak)
-    {
         g_MasterNackFlag = true;
-    }
     else
-    {
         g_MasterCompletionFlag = true;
-        /* Display failure information when status is not success. */
-        if (status != kStatus_Success)
-        {
-            PRINTF("Error occured during transfer!");
-        }
-    }
 }
 
-/*!
- * @brief Main function
- */
-int main(void)
+/* Write single register */
+void MPU_WriteReg(uint8_t reg, uint8_t value)
 {
     lpi2c_master_transfer_t masterXfer = {0};
-    status_t reVal                     = kStatus_Fail;
+    uint8_t data[2] = {reg, value};
 
+    masterXfer.slaveAddress = MPU9250_ADDR;
+    masterXfer.direction = kLPI2C_Write;
+    masterXfer.data = data;
+    masterXfer.dataSize = 2;
+    masterXfer.flags = kLPI2C_TransferDefaultFlag;
+
+    LPI2C_MasterTransferBlocking(LPI2C_MASTER_BASE, &masterXfer);
+}
+
+/* Read bytes from MPU */
+void MPU_ReadRegs(uint8_t reg, uint8_t *data, uint8_t length)
+{
+    lpi2c_master_transfer_t masterXfer = {0};
+
+    masterXfer.slaveAddress = MPU9250_ADDR;
+    masterXfer.direction = kLPI2C_Read;
+    masterXfer.subaddress = reg;
+    masterXfer.subaddressSize = 1;
+    masterXfer.data = data;
+    masterXfer.dataSize = length;
+    masterXfer.flags = kLPI2C_TransferDefaultFlag;
+
+    LPI2C_MasterTransferBlocking(LPI2C_MASTER_BASE, &masterXfer);
+}
+
+int main(void)
+{
     BOARD_InitHardware();
-
-    PRINTF("\r\nLPI2C board2board interrupt example -- Master transfer.\r\n");
-
-    /* Set up i2c master to send data to slave*/
-    /* First data in txBuff is data length of the transmiting data. */
-    g_master_txBuff[0] = I2C_DATA_LENGTH - 1U;
-    for (uint32_t i = 1U; i < I2C_DATA_LENGTH; i++)
-    {
-        g_master_txBuff[i] = i - 1;
-    }
-
-    PRINTF("Master will send data :");
-    for (uint32_t i = 0U; i < I2C_DATA_LENGTH - 1; i++)
-    {
-        if (i % 8 == 0)
-        {
-            PRINTF("\r\n");
-        }
-        PRINTF("0x%2x  ", g_master_txBuff[i + 1]);
-    }
-    PRINTF("\r\n\r\n");
-
+    LPI2C_MasterGetDefaultConfig(&(lpi2c_master_config_t){0});
     lpi2c_master_config_t masterConfig;
-
-    /*
-     * masterConfig.debugEnable = false;
-     * masterConfig.ignoreAck = false;
-     * masterConfig.pinConfig = kLPI2C_2PinOpenDrain;
-     * masterConfig.baudRate_Hz = 100000U;
-     * masterConfig.busIdleTimeout_ns = 0;
-     * masterConfig.pinLowTimeout_ns = 0;
-     * masterConfig.sdaGlitchFilterWidth_ns = 0;
-     * masterConfig.sclGlitchFilterWidth_ns = 0;
-     */
     LPI2C_MasterGetDefaultConfig(&masterConfig);
-
-    /* Change the default baudrate configuration */
     masterConfig.baudRate_Hz = I2C_BAUDRATE;
+    LPI2C_MasterInit(LPI2C_MASTER_BASE, &masterConfig, CLOCK_GetFreq(LPI2C_MASTER_CLOCK_FREQUENCY));
 
-    /* Initialize the LPI2C master peripheral */
-    LPI2C_MasterInit(EXAMPLE_I2C_MASTER, &masterConfig, LPI2C_MASTER_CLOCK_FREQUENCY);
+    PRINTF("Initializing MPU9250...\r\n");
 
-    /* Create the LPI2C handle for the non-blocking transfer */
-    LPI2C_MasterTransferCreateHandle(EXAMPLE_I2C_MASTER, &g_m_handle, lpi2c_master_callback, NULL);
+    /* Reset and wake up MPU */
+    MPU_WriteReg(PWR_MGMT_1_REG, 0x80);
+    SDK_DelayAtLeastUs(100000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
+    MPU_WriteReg(PWR_MGMT_1_REG, 0x00);
+    SDK_DelayAtLeastUs(100000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
 
-    /* subAddress = 0x01, data = g_master_txBuff - write to slave.
-      start + slaveaddress(w) + subAddress + length of data buffer + data buffer + stop*/
-    uint8_t deviceAddress     = 0x01U;
-    masterXfer.slaveAddress   = I2C_MASTER_SLAVE_ADDR_7BIT;
-    masterXfer.direction      = kLPI2C_Write;
-    masterXfer.subaddress     = (uint32_t)deviceAddress;
-    masterXfer.subaddressSize = 1;
-    masterXfer.data           = g_master_txBuff;
-    masterXfer.dataSize       = I2C_DATA_LENGTH;
-    masterXfer.flags          = kLPI2C_TransferDefaultFlag;
+    /* Verify connection */
+    uint8_t whoami = 0;
+    MPU_ReadRegs(WHO_AM_I_REG, &whoami, 1);
+    PRINTF("WHO_AM_I = 0x%X\r\n", whoami);
 
-    /* Send master non-blocking data to slave */
-    reVal = LPI2C_MasterTransferNonBlocking(EXAMPLE_I2C_MASTER, &g_m_handle, &masterXfer);
+    uint8_t sensorData[14];
+    int16_t ax, ay, az, gx, gy, gz, temp;
+    PRINTF("Calibrating MPU...\r\n");
+    for (int i = 0; i < CALIB_SAMPLE; i++) {
+    	MPU_ReadRegs(ACCEL_XOUT_H, sensorData, 14);
+        ax = (sensorData[0] << 8) | sensorData[1];
+        ay = (sensorData[2] << 8) | sensorData[3];
+        az = (sensorData[4] << 8) | sensorData[5];
+        temp = (sensorData[6] << 8) | sensorData[7];
+        gx = (sensorData[8] << 8) | sensorData[9];
+        gy = (sensorData[10] << 8) | sensorData[11];
+        gz = (sensorData[12] << 8) | sensorData[13];
 
-    if (reVal != kStatus_Success)
-    {
-        return -1;
-    }
-    /*  Wait for transfer completed. */
-    while ((!g_MasterCompletionFlag) && (!g_MasterNackFlag))
-    {
-    }
-    if (g_MasterNackFlag)
-    {
-        PRINTF("Master nacked by slave!");
-        g_MasterNackFlag = false;
-    }
-    g_MasterCompletionFlag = false;
+        ax_sum += ax;
+        ay_sum += ay;
+        az_sum += az;
+        temp_sum += temp;
+        gx_sum += gx;
+        gy_sum += gy;
+        gz_sum += gz;
+	}
 
-    PRINTF("Receive sent data from slave :");
+    int32_t ax_offset =((ax_sum-(ax_sum*0.05))/CALIB_SAMPLE);
+    int32_t ay_offset =((ay_sum-(ay_sum*0.05))/CALIB_SAMPLE);
+    int32_t az_offset =((az_sum-(az_sum*0.05))/CALIB_SAMPLE);
+    int32_t temp_offset =((temp_sum-(temp_sum*0.05))/CALIB_SAMPLE);
+    int32_t gx_offset =((gx_sum-(gx_sum*0.05))/CALIB_SAMPLE);
+    int32_t gy_offset =((gy_sum-(gy_sum*0.05))/CALIB_SAMPLE);
+    int32_t gz_offset =((gz_sum-(gz_sum*0.05))/CALIB_SAMPLE);
 
-    /* subAddress = 0x01, data = g_master_rxBuff - read from slave.
-      start + slaveaddress(w) + subAddress + repeated start + slaveaddress(r) + rx data buffer + stop */
-    masterXfer.slaveAddress   = I2C_MASTER_SLAVE_ADDR_7BIT;
-    masterXfer.direction      = kLPI2C_Read;
-    masterXfer.subaddress     = (uint32_t)deviceAddress;
-    masterXfer.subaddressSize = 1;
-    masterXfer.data           = g_master_rxBuff;
-    masterXfer.dataSize       = I2C_DATA_LENGTH - 1;
-    masterXfer.flags          = kLPI2C_TransferDefaultFlag;
-
-    reVal = LPI2C_MasterTransferNonBlocking(EXAMPLE_I2C_MASTER, &g_m_handle, &masterXfer);
-    if (reVal != kStatus_Success)
-    {
-        return -1;
-    }
-
-    /*  Wait for transfer completed. */
-    while ((!g_MasterCompletionFlag) && (!g_MasterNackFlag))
-    {
-    }
-    if (g_MasterNackFlag)
-    {
-        PRINTF("Master nacked by slave!");
-        g_MasterNackFlag = false;
-    }
-    g_MasterCompletionFlag = false;
-
-    for (uint32_t i = 0U; i < I2C_DATA_LENGTH - 1; i++)
-    {
-        if (i % 8 == 0)
-        {
-            PRINTF("\r\n");
-        }
-        PRINTF("0x%2x  ", g_master_rxBuff[i]);
-    }
-    PRINTF("\r\n\r\n");
-
-    /* Transfer completed. Check the data.*/
-    for (uint32_t i = 0U; i < I2C_DATA_LENGTH - 1; i++)
-    {
-        if (g_master_rxBuff[i] != g_master_txBuff[i + 1])
-        {
-            PRINTF("\r\nError occurred in the transfer ! \r\n");
-            break;
-        }
-    }
-
-    PRINTF("\r\nEnd of LPI2C example .\r\n");
+    PRINTF("\n\nOFFSETs\nACCEL: X=%d Y=%d Z=%d | GYRO: X=%d Y=%d Z=%d | TEMP=%d\r\n\n\n", ax_offset, ay_offset,
+    		az_offset, gx_offset, gy_offset, gz_offset, temp_offset);
     while (1)
     {
+        MPU_ReadRegs(ACCEL_XOUT_H, sensorData, 14);
+
+        ax = ((sensorData[0] << 8) | sensorData[1]) - ax_offset;
+        ay = ((sensorData[2] << 8) | sensorData[3]) - ay_offset;
+        az = ((sensorData[4] << 8) | sensorData[5]) - az_offset;
+        temp = ((sensorData[6] << 8) | sensorData[7]) - temp_offset;
+        gx = ((sensorData[8] << 8) | sensorData[9]) - gx_offset;
+        gy = ((sensorData[10] << 8) | sensorData[11]) - gy_offset;
+        gz = ((sensorData[12] << 8) | sensorData[13]) - gz_offset;
+
+        PRINTF("ACCEL: X=%d Y=%d Z=%d | GYRO: X=%d Y=%d Z=%d | TEMP=%d\r\n", ax, ay, az, gx, gy, gz, temp);
+
+        SDK_DelayAtLeastUs(100000, CLOCK_GetFreq(kCLOCK_CoreSysClk)); // 100ms
     }
 }
